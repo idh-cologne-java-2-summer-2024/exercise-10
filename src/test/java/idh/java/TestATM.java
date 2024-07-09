803c9f32997fdf76ae0e1ce234aaf21cf1757327
Marc Tetsuo Benscheid <mbensche@smail.de>
2024-07-09 12:50:27 +0200

    small corrections and added @DisplayName Annotations to Test-Methods

--- src/test/java/idh/java/TestATM.java
+++ src/test/java/idh/java/TestATM.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import idh.java.ATM.IllegalInputException;
@@ -17,13 +18,14 @@ public class TestATM {
 	int[] testMinCash = new int[7];
 	int huge = 2147483647;
 	int tiny = -2147483647;
-
+	
+	//Creating a new static ATM Object to work with, needs to be static since BeforeAll is intended to run first before any Test Instances are created
 	@BeforeAll
 	public static void setUpATM() {
 		test1 = new ATM();
 	}
 	
-	
+	//Playing around with @BeforeEach-Annotions, this is not needed
 	@BeforeEach
 	void Prep() {
 		for (int i : testMaxCash) {
@@ -39,14 +41,15 @@ public class TestATM {
 		}
 	}
 //	Test Methods are always void, without parameters in () 
-//	--> from Praktikum 09.07.24, does not work yet since convertToBills is protected, renaming the package does the trick
+//	--> from Praktikum 09.07.24, does not work yet since convertToBills is protected, nvm renaming the package did the trick
 	@Test
+//	@DisplayName("Should run the convertToBills-Method with a big Integer and return an Array with 0s")
 	public void testMinAndMaxInput() {
 		
 		try {
 			
 			int[] assertedMinimum = {0,0,0,0,0,0,0};
-			int[] resultMinimum = test1.convertToBills(huge);
+			int[] resultMinimum = test1.convertToBills(tiny);
 			assertArrayEquals(assertedMinimum, resultMinimum);
 			
 			int inputMax = 2147483645;
@@ -59,25 +62,30 @@ public class TestATM {
 		}
 		
 	}
-	
+	/**
+	 * Testing against negative inputs to the <code>convertToBills()-method</code>
+	 */
 	@Test
+	@DisplayName("Should throw an IllegalInputException due to a negative Integer passed to the convertToBills-Method")
 	void NegativeTestInput() {
 //		Prep();
 		int[] asserted = {0,0,0,0,0,0,0};
 		int[] result;
 		try {
-			result = test1.convertToBills(6);
+			result = test1.convertToBills(-5);
 			assertArrayEquals(asserted,result);
 		} catch (IllegalInputException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 //		assertArrayEquals(tiny, test1.cashout(tiny)); //tiny is no array
 	}
 	
-	
+	/**
+	 * Testing against illegal inputs, triggering the self-written (not by me) <code>IllegalInputException</code>
+	 */
 	@Test
+	@DisplayName("Should throw an IllegalInputException")
 	public void testIllegalInput() {
 		assertThrows(IllegalInputException.class, () -> test1.convertToBills(6)); // ()-> code to be executed as an executable
 		assertThrows(IllegalInputException.class, () -> test1.convertToBills(12));
